f7634231a837fe77a5260cdf70175efce9674190
floschne <flows@web.de>
2017-12-25 18:43:11 +0100

    dummy implementation to get the CMUDICT data in python

--- src/main/main.py
+++ src/main/main.py
@@ -1,1 +1,24 @@
-print('hello world')
+import os
+import tensorflow as tf
+
+
+def prepare_data(path_to_dict='../../data/cmudict/cmudict.dict'):
+    assert os.path.isfile(path_to_dict), 'Cannot find cmudict file at: ' + path_to_dict
+
+    cmudict_raw = open(path_to_dict).readlines()
+    cmudict = {}
+    for line in cmudict_raw:
+        line = line.split()
+        cmudict[line[0]] = line[1:]
+
+    assert len(cmudict) == len(cmudict_raw), 'Error while reading dictionary!'
+
+    return cmudict
+
+
+def build_model():
+
+    return
+
+cmudict = prepare_data()
+print(cmudict['tree'])
